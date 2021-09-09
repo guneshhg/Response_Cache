de72b86430bcbc7a97e57715496d45f3b9e69156
RPK\hgunes <h.gunes@krosotomotiv.com.tr>
2021-09-09 11:46:05 +0300

    Response_Caching

---
+++ ResponseCachingSample/ResponseCachingSample/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResponseCachingSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [ResponseCache(Duration = 30)]
+        public IEnumerable<string> Get()
+        {
+            var time = "The response time is:" + DateTime.Now.ToString();
+            return new string[] { "CachedItems", time };
+        }
+
+
+    }
+}
